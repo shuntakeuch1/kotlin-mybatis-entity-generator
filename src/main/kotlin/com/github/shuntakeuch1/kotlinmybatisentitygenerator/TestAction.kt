c07cea179d9928b200c8b7006276fdec18ffaf8c
shuntakeuch1 <takeuchishun89@gmail.com>
2020-07-25 14:09:37 +0900

    delete comment

--- src/main/kotlin/com/github/shuntakeuch1/kotlinmybatisentitygenerator/TestAction.kt
+++ src/main/kotlin/com/github/shuntakeuch1/kotlinmybatisentitygenerator/TestAction.kt
@@ -10,17 +10,7 @@ class TestAction() : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.getData(PlatformDataKeys.PROJECT)
         if (TestDialog().showAndGet()) {
-            println("success")
-        }
-//        SampleDialogWrapper().show()
-//        val txt = Messages.showInputDialog(project, "What is your name?", "Input your name", Messages.getQuestionIcon())
-//
-//        val tables = MySQLRepository().getTables()
-//        val eg = EntityGenerator()
-//        if (txt != null){
-//            eg.targetDirectory = txt
-//        }
-//        eg.execute(tables)
-//        Messages.showMessageDialog(project, "Hello, $txt!\n I am glad to see you.", "Information", Messages.getInformationIcon())
+            println("end form")
+        } 
     }
 }
