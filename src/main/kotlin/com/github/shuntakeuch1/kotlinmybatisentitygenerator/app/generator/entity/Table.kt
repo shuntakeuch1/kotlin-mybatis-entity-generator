e1da60211c94394f25bff0d21ca16200bdc9d595
shuntakeuch1 <takeuchishun89@gmail.com>
2020-08-01 21:21:37 +0900

    add table string for test code

--- src/main/kotlin/com/github/shuntakeuch1/kotlinmybatisentitygenerator/app/generator/entity/Table.kt
+++ src/main/kotlin/com/github/shuntakeuch1/kotlinmybatisentitygenerator/app/generator/entity/Table.kt
@@ -3,4 +3,10 @@ package com.github.shuntakeuch1.kotlinmybatisentitygenerator.app.generator.entit
 class Table(
     val name: String,
     val columns: Array<Column>
-)
+) {
+    fun toColumnAllString(): String {
+        return columns.map { column ->
+            "\n val " + column.field.toString() + ": " + column.typeConverter()
+        }.joinToString(",")
+    }
+}
---
+++ src/test/kotlin/com/github/shuntakeuch1/kotlinmybatisentitygenerator/app/generator/entity/TableTest.kt
@@ -0,0 +1,46 @@
+package com.github.shuntakeuch1.kotlinmybatisentitygenerator.app.generator.entity
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class TableTest {
+
+    @Test
+    fun testExecute() {
+        val table = Table(
+            name = "",
+            columns = arrayOf<Column>(
+                Column(
+                    field = "",
+                    type = "varchar(10)",
+                    nullFlag = "",
+                    key = "",
+                    defaultFlag = "",
+                    extra = ""
+                ),
+                Column(
+                    field = "",
+                    type = "int(11)",
+                    nullFlag = "",
+                    key = "",
+                    defaultFlag = "",
+                    extra = ""
+                ),
+                Column(
+                    field = "",
+                    type = "timestamp",
+                    nullFlag = "",
+                    key = "",
+                    defaultFlag = "",
+                    extra = ""
+                )
+            )
+        )
+        val expect = """
+             val : String,
+             val : Int,
+             val : LocalDateTime
+        """.trimIndent()
+        assertEquals(expect, table.toColumnAllString().trimIndent())
+    }
+}
