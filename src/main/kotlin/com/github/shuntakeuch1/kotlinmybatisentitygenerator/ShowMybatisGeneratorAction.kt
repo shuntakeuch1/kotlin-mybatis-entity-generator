2405c57bf9d17e93242a12df400e69911af509a1
shuntakeuch1 <takeuchishun89@gmail.com>
2020-08-16 22:35:43 +0900

    sample count

--- src/main/kotlin/com/github/shuntakeuch1/kotlinmybatisentitygenerator/ShowMybatisGeneratorAction.kt
+++ src/main/kotlin/com/github/shuntakeuch1/kotlinmybatisentitygenerator/ShowMybatisGeneratorAction.kt
@@ -8,6 +8,17 @@ import com.intellij.openapi.actionSystem.PlatformDataKeys
 class ShowMybatisGeneratorAction : AnAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
+
+        val config = PluginConfig.getInstance(e.project)
+
+        if (config != null) {
+            if (config.isEmpty) {
+                config.init()
+                println("init count")
+            }
+            println("start plugin " + config.count)
+            config.increment()
+        }
         val project = e.getData(PlatformDataKeys.PROJECT)
         if (GeneratorDialog(project).showAndGet()) {
             println("end form")
